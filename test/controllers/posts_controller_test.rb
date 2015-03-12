b5a5128dbc8ae3b7ac8f9ab17b00505d12914a23
kangkyu <kangkyu1111@gmail.com>
2015-03-12 13:12:53 -0700

    refactor posts controller test with setup method

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # setup do
-  #   @post = posts(:one)
-  # end
+  def setup
+    @post = posts(:one)
+  end
 
   test "should get index" do
     get :index
@@ -12,26 +12,26 @@ class PostsControllerTest < ActionController::TestCase
   end
 
   test "should show post" do
-    get :show, id: posts(:one).id
+    get :show, id: @post.id
     assert_response :success
   end
 
   test "should create post" do
     assert_difference('Post.count', 1) do
-      post :create, post: { title: "Hello World", body: "Hello KangKyu, This is the world." }
+      post :create, post: { title: @post.title, body: @post.body }
     end
 
     assert_redirected_to post_path(assigns(:post))
   end
 
   test "should update post" do
-    patch :update, id: posts(:one).id, post: { title: "Hello World", body: "Hello KangKyu, This is the world." }
+    patch :update, id: @post.id, post: { title: @post.title, body: @post.body }
     assert_redirected_to post_path(assigns(:post))
   end
 
   test "should destroy post" do
     assert_difference('Post.count', -1) do
-      delete :destroy, id: posts(:one).id
+      delete :destroy, id: @post.id
     end
     assert_redirected_to posts_path
   end
